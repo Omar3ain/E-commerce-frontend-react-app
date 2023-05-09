f72ef39c39f906478b78587c7bbcb602cc3b9d85
muhannad ibrahim <muhannad.ibrahim2311@gmail.com>
2023-05-10 01:23:50 +0300

    delete unnecessary comments from store file

--- src/store.js
+++ src/store.js
@@ -5,22 +5,6 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 // Reducers
 import authReducer from './features/auth/authSlice';
-// import { authReducer } from './reducers/userReducers';
-
-// const reducer = combineReducers({
-//     auth: authReducer,
-// });
-
-// let initialState = {};
-
-// const middleware = [thunk];
-
-// const store = configureStore({
-//     reducer,
-//     initialState
-// });
-
-// export default store;
 
 const store = configureStore({
     reducer: {
