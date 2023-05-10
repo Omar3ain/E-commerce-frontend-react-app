3ab1c5651c0c879d6eed380eaf2cee41c6c65208
omar3ain <omar3ain@gmail.com>
2023-05-10 18:32:47 +0300

    made navbar responsive

--- src/components/homePage/Navbar/NavbarComponent.jsx
+++ src/components/homePage/Navbar/NavbarComponent.jsx
@@ -1,26 +1,38 @@
+import { useState } from 'react';
 import styles from './Navbar.module.css';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+
 
 
 
 function NavbarComponent() {
+  const [active,setActive] = useState(false);
+
   return (
-    <>
-    <Navbar bg="light" variant="light" className={styles['navbar-pos']}>
-        <Container>
-          <a href=""className={styles['logo']}><span>W</span>ebsite</a>
-          <Nav>
-            <Nav.Link href="#home"style={{ color: '#2D2A32',fontSize:'18px'}} >Home</Nav.Link>
-            <Nav.Link href="#features" style={{ color: '#2D2A32',fontSize:'18px' }} >Products</Nav.Link>
-            <Nav.Link href="#pricing" style={{ color: '#2D2A32',fontSize:'18px' }} >Pricing</Nav.Link>
-            <Nav.Link href="/login" style={{ color: '#2D2A32',fontSize:'18px' }} >Login</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-  </>
-  )
+      <div className={styles['navContainer']}>
+        <p className={styles['logo']}><span>W</span>ebsite</p>
+        <div className={styles['hamburger']}>
+        <input type="checkbox" className={styles['hamburger-init']}
+          checked={active}
+          onChange={() => setActive(!active)}
+        />
+          <div className={styles['menu']}>
+            <div className={styles['bar1']}></div>
+            <div className={styles['bar2']}></div>
+            <div className={styles['bar3']}></div>
+          </div>
+        </div> 
+
+
+            <ul className={`${styles['list']} ${active ? styles['isActive'] : ''} ${active ? styles['newClass'] : ''}`}>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#features">Products</a></li>
+              <li><a href="#pricing">Pricing</a></li>
+              <li><a href="/login">Login</a></li>
+            </ul>
+      </div>
+
+  );
 }
 
 export default NavbarComponent;
