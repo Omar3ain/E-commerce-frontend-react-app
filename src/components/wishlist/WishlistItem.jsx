b088a9b163e07b336cd34f40336ede454011fbc8
somayaax <somayaa192@gmail.com>
2023-05-18 18:43:55 +0300

    add pagination and search

--- src/components/wishlist/WishlistItem.jsx
+++ src/components/wishlist/WishlistItem.jsx
@@ -5,6 +5,7 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useDispatch } from 'react-redux';
 import { removeFromWishlist } from '../../features/wishlist/wishlistSlice';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 const WishlistItem = ({ item }) => {
     const dispatch = useDispatch()
@@ -31,9 +32,10 @@ const WishlistItem = ({ item }) => {
                 <CardContent sx={{ display: 'flex', alignItems: 'center', color: '#85822E' }} className={styles['item-details']}>
 
                     <Box sx={{ display: 'flex', alignItems: 'center', paddingLeft: '1.5rem' }} className={styles['item-quantity']}>
-                        <Button variant="contained" onClick={() => { dispatch(addToCart(item.product.id)) }} style={{ backgroundColor: 'white', color: '#85822E' }}>
+                    {item.product.quantity? <Button variant="contained" onClick={() => { dispatch(addToCart(item.product.id)) }} style={{ backgroundColor: 'white', color: '#85822E' }}>
                             <ShoppingCartIcon />
-                            add to cart</Button>
+                            add to cart</Button>:
+                             <Typography style={{color:'red'}}><ErrorOutlineIcon style={{color:'red', fontSize:'1.4rem'}}/>Out Of Stock</Typography>}
                         <Button variant="contained" onClick={() => { dispatch(removeFromWishlist(item.product.id)) }} title="remove item" style={{ backgroundColor: 'white', color: '#85822E', marginLeft:'1rem' }}>
                             <DeleteOutlineIcon />
                         </Button>
--- src/pages/ProductsPage/Products.jsx
+++ src/pages/ProductsPage/Products.jsx
@@ -1,32 +1,52 @@
-import { Container, Grid } from "@mui/material"
+import { Button, Container, Grid, TextField } from "@mui/material"
 import styles from './products.module.css';
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { getProducts } from "../../features/product/productSlice";
+import { useEffect, useRef, useState } from "react";
+import { getNextPage, getPrevPage, getProducts } from "../../features/product/productSlice";
 import Filters from "../../components/products/Filters/Filters";
 import ProductItem from "../../components/products/index.js";
 import { getWishlist } from "../../features/wishlist/wishlistSlice";
 import { useParams } from 'react-router-dom';
+import debounce from "lodash.debounce";
 
 const Products = () => {
     const dispatch = useDispatch();
     const { categoryId } = useParams();
-    useEffect(() => {
-        dispatch(getProducts(categoryId))
-        dispatch(getWishlist())
-    }, [dispatch])
+    const [filter, setFilter] = useState('')
+    const [key, setKey] = useState('')
 
-    const { products, isLoading } = useSelector((state) => state.product)
+    const debouncedSetFilter = useRef(debounce((value) => {
+        setFilter(value);
+    }, 1000)).current;
+    const handleChange = (e) => {
+        const { value } = e.target;
+        setKey(value);
+        debouncedSetFilter(value);
+    };
+
+    const { products, next, previous, isLoading } = useSelector((state) => state.product)
     const wishlist = useSelector((state) => state.wishlist);
-    const wishlistProductIds = wishlist.wishlistItems.map(item => item.product.id )
-
+    const wishlistProductIds = wishlist.wishlistItems.map(item => item.product.id)
+    useEffect(() => {
+        dispatch(getProducts({ categoryId, search: filter }))
+        dispatch(getWishlist())
+    }, [dispatch, filter , categoryId])
     return (
-        <div className={styles['products']}>
-            <Filters />
-            <Container maxWidth="xl" sx={{ marginTop: 3, marginBottom: 3 }} >
-            {isLoading ? (
-                <h2 style={{ textAlign: 'center' }}>Loading...</h2>
-            ) : products.length === 0 ? (
+        <>
+            <div className={styles['products']}>
+                <Filters />
+                <Container maxWidth="xl" sx={{ marginTop: 3, marginBottom: 3 }} >
+                    <TextField
+                        id="outlined-controlled"
+                        label="Search in products"
+                        fullWidth
+                        value={key}
+                        onChange={handleChange}
+                        sx={{ marginBottom: 5 }}
+                    />
+                    {isLoading ? (
+                        <h2 style={{ textAlign: 'center' }}>Loading...</h2>
+                    ) : products.length === 0 ? (
                         <h2 style={{ textAlign: 'center' }}>No products to display</h2>
                     ) : (
                         <Grid container spacing={{ xs: 1 }} columns={{ xs: 4, sm: 8, md: 12 }}>
@@ -37,9 +57,21 @@ const Products = () => {
                             ))}
                         </Grid>
                     )
-                }
-            </Container>
-        </div>
+                    }
+                </Container>
+            </div>
+            <nav aria-label="Page navigation example">
+                <ul className="pagination justify-content-center mt-4">
+                    <li className="page-item">
+                        <Button disabled={!previous} variant="outlined" className="page-link" onClick={() => dispatch(getPrevPage(previous))}>Previous</Button>
+                    </li>
+                    {/* <li><a className="page-link">1</a></li> */}
+                    <li className="page-item">
+                        <Button disabled={!next} variant="outlined" className="page-link" onClick={() => dispatch(getNextPage(next))}>Next</Button>
+                    </li>
+                </ul>
+            </nav>
+        </>
 
     )
 }
